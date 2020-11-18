05b19e6a9714f6f7400e5bf3df9f7766c92d2cd8
Nikki Freeman <nlbfreeman@gmail.com>
2020-11-18 15:08:03 -0600

    Function for responder status based on normal quantiles rather than sample quantiles

--- Code/stage1_responderStatus.R
+++ Code/stage1_responderStatus.R
@@ -8,7 +8,7 @@
 #' @param ctsOutcome1 character; name of the cts cut-off variable
 #' @param cutoffs vector of quantiles (e.g. c(0.2, 0.8))
 #'
-#' @return dataframe wtih the responder stsatus appended
+#' @return dataframe wtih the responder status appended
 assignResponderStatusByQuantile <- function(df, ctsOutcome1, cutoffs){
   out <- df %>%
     group_by(A1) %>%
@@ -16,5 +16,22 @@ assignResponderStatusByQuantile <- function(df, ctsOutcome1, cutoffs){
     mutate(respStatus = if_else(pctRank <= cutoffs[1], "bad", "medium"),
            respStatus = if_else(pctRank >= cutoffs[2], 
                                 "good", respStatus))
+  return(out)
+}
+
+#' Assign responder status by quantile from the N(0, 1)
+#'
+#' @param df dataframe; each row corresponds to a ppts and has Y1
+#' @param cutoffs vector of quantiles (e.g. c(0.2, 0.8))
+#'
+#' @return dataframe with responder status appended
+#' @export
+#'
+#' @examples
+assignResponderStatusByNormalQuantileForStdNormalY1 <- function(df, cutoffs){
+  out <- df %>%
+    mutate(respStatus = if_else(Y1 <= qnorm(cutoffs[1]), "bad", "medium")) %>%
+    mutate(respStatus = if_else(Y1 >= qnorm(cutoffs[1]), "good", respStatus))
+  
   return(out)
 }
