f20a34b1d2267a7fbb85b09d6a4373ee62f5b184
Nikki Freeman <nlbfreeman@gmail.com>
2020-11-18 14:12:53 -0600

    Added functions *_v2 to balance allocation at stage 1 and stage 2

--- Code/stage1_treatment_allocation.R
+++ Code/stage1_treatment_allocation.R
@@ -16,3 +16,22 @@ allocationFn_stage1_v1 <- function(df, firstLineTreatments){
     ungroup()
   return(out)
 }
+
+#' Randomly assign the first line treatments; equivelent to blocked randomization
+#'
+#' @param df dataframe; each row contains a ppt
+#' @param firstLineTreatments character vector of the first line treatments
+#'
+#' @return dataframe with the first line treatment assignemnts appended;
+#' This function allocates the treatments evenly. There is no need to randomize
+#' within the blocks for the simulation; as written this is approximately 
+#' the allocation one would get with block randomization
+allocationFn_stage1_v2 <- function(df, firstLineTreatments){
+
+  out <- df %>% 
+    mutate(A1 = rep_len(firstLineTreatments, length.out = nrow(df)))
+  
+  return(out)
+}
+
+
--- Code/stage2_treatment_allocation.R
+++ Code/stage2_treatment_allocation.R
@@ -53,3 +53,119 @@ allocationFn_stage2_v1 <- function(df, firstLineTreatments,
   
   return(out)
 }
+
+
+#' Helper function to balance 2nd treatment allocation for those who respond bad
+#'
+#' @param A1 string; first line treatment assigned
+#' @param n integer; number of people assigned to A1
+#' @param data dataframe; filtered to those that got A1
+#' @param secondLineTreatments character vector; possible second line treatments
+#'
+#' @return dataframe; data with the second treatment assignments appended
+#' @export
+#'
+#' @examples
+allocation2helper1 <- function(A1, n, data, secondLineTreatments){
+  feasibleSecondLine <- secondLineTreatments[secondLineTreatments != A1]
+  out <- data %>% mutate(A2 = rep_len(gtools::permute(feasibleSecondLine), length.out = n))
+  return(out)
+}
+
+#' Helper function to balance 2nd treatment allocation for those who respond med and had A1 == SOC
+#'
+#' @param n integer; number of people assigned to A1 = SOC
+#' @param data dataframe; people with A1 = SOC and response = medium
+#' @param secondLineTreatments character vector; second line treatments
+#' @param augmentationTreatments character vector; augmentation treatments
+#' @param standardOfCareTreatment character; standard of care treatment
+#'
+#' @return dataframe; data wtih A2 appended
+#' @export
+#'
+#' @examples
+allocation2helper2 <- function(n, data, secondLineTreatments, augmentationTreatments, standardOfCareTreatment){
+  feasibleSecondLine <- c(secondLineTreatments, augmentationTreatments, standardOfCareTreatment)
+  out <- data %>% mutate(A2 = rep_len(gtools::permute(feasibleSecondLine), length.out = n))
+  return(out)
+}
+
+#' Helperfunction to balance 2nd treatment allocation for those who respond med and A1 != SOC
+#'
+#' @param data dataframe; people with A1 = a1 and response = medium
+#' @param n integer; number of people with A1 = a1 and response = medium
+#' @param secondLineTreatments character vector; second line treatments
+#' @param augmentationTreatments character vector; augmentation treatments
+#'
+#' @return dataframe; data with A2 appended
+#' @export
+#'
+#' @examples
+allocation2helper3 <- function(data, n, secondLineTreatments, augmentationTreatments){
+  feasibleSecondLine <- c(secondLineTreatments, augmentationTreatments)
+  out <- data %>% mutate(A2 = rep_len(gtools::permute(feasibleSecondLine), length.out = n))
+  return(out)
+}
+
+#' Allocate stage 2 treatments (equivalent to blocking)
+#'
+#' @param df dataframe; contain the ppts, first line treatments, and responder status
+#' @param firstLineTreatments character vector; firstline treatments
+#' @param secondLineTreatments character vector; secondline treatments
+#' @param augmentationTreatments character vector; augmentation treatments
+#' @param standardOfCareTreatment character; standard of care treatment
+#'
+#' @return dataframe with A2 appended; Given the feasible treatments after accounting
+#' for A1 and responder status, A2 is allocationed evenly across feasible treatments.
+#' @export
+#'
+#' @examples
+allocationFn_stage2_v2 <- function(df, firstLineTreatments,
+                                   secondLineTreatments,
+                                   augmentationTreatments,
+                                   standardOfCareTreatment){
+  # Available treatments depend on the response status
+  # Consider each response group separately
+  # Good is the easiest case, just continue on with A1
+  good <- df %>% filter(respStatus == "good") %>%
+    mutate(A2 = A1)
+  
+  # Bad is the second easiest case, switch off of A1 and no augmentation
+  bad <- df %>% filter(respStatus == "bad") %>%
+    group_by(A1) %>%
+    add_count() %>%
+    group_by(A1, n) %>%
+    nest() %>%
+    mutate(data = map(data, allocation2helper1, A1 = A1, n = n, 
+                      secondLineTreatments = secondLineTreatments)) %>%
+    unnest(data) %>%
+    ungroup() %>%
+    select(names(good))
+    
+  medium_soc <- df %>% filter(respStatus == "medium" & A1 == standardOfCareTreatment) %>%
+    add_count() %>%
+    group_by(A1, n) %>%
+    nest() %>%
+    mutate(data = map(data, allocation2helper2, n = n, secondLineTreatments = secondLineTreatments,
+                      augmentationTreatments = augmentationTreatments, 
+                      standardOfCareTreatment = standardOfCareTreatment)) %>%
+    unnest(data) %>%
+    ungroup() %>%
+    select(names(good))
+  
+  medium <- df %>% filter(respStatus == "medium" & A1 != standardOfCareTreatment) %>%
+    group_by(A1) %>%
+    add_count() %>%
+    group_by(A1, n) %>%
+    nest() %>%
+    mutate(data = map(data, allocation2helper3, n = n, secondLineTreatments = secondLineTreatments, 
+                      augmentationTreatments = augmentationTreatments)) %>%
+    unnest(data) %>%
+    ungroup() %>%
+    select(names(good))
+  
+  out <- bind_rows(good, bad, medium_soc, medium) %>%
+    arrange(ptid)
+  
+  return(out)
+}
