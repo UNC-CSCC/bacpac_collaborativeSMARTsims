f01ea15b4113ebb722debdd1fdcbaa90ad4974ff
Nikki Freeman <nlbfreeman@gmail.com>
2020-11-18 19:06:25 -0600

    Changed pivot_wider approach per John's recommendation

--- Code/stage1_outcome.R
+++ Code/stage1_outcome.R
@@ -12,23 +12,23 @@
 #'
 #' @return dataframe with Y1 appended
 generateNormalY1 <- function(df, intercept, coefs, sigma){
-  # coef_sym <- syms(names(coefs))
-  # summands <- map2(coef_sym, coefs, ~ expr((!!.x * !!.y)))
-  # summands <- c(intercept, summands)
-  # eq <- reduce(summands, ~ expr(!!.x + !!.y))
-  # eq <- linearPredictor(intercept, coefs)
   
-  # out <- df %>% 
-  #   mutate(mu := !!eq) %>%
-  #   rowwise() %>%
-  #   mutate(Y1 = rnorm(n = 1, mean = mu, sd = sigma)) %>%
-  #   ungroup() %>%
-  #   select(-mu)
+  # Get the unique treatments
+  treatments <- unique(df$A1)
+  treatments <- paste("A1", treatments, sep = "_")
   
-  out <- df %>% mutate(temp = 1, A1_copy = A1) %>%
-    pivot_wider(names_from = A1_copy, names_prefix = "A1_", values_from = temp) %>%
-    mutate_at(vars(-group_cols()), ~replace(., is.na(.), 0)) %>%
-    mutate(mu := !!(linearPredictor2(intercept = 0, 
+  # Create placeholders for treatment dummies and add to df
+  treatment_df <- data.frame(matrix(0, ncol = length(treatments), nrow = nrow(df)))
+  names(treatment_df) <- treatments
+  df <- bind_cols(df, treatment_df)
+  
+  out <- df %>%
+    group_by(A1) %>%
+    nest() %>%
+    mutate(data = map(data, makeDummyHelper, A1 = A1)) %>%
+    unnest(data) %>%
+    ungroup() %>%
+    mutate(mu := !!(linearPredictor2(intercept = 0,
                                      coefs = coefs))) %>%
     mutate(mu = mu + intercept) %>%
     rowwise() %>%
@@ -36,8 +36,26 @@ generateNormalY1 <- function(df, intercept, coefs, sigma){
     ungroup() %>%
     select(-mu) %>%
     select(-starts_with("A1_"))
+    
+  # out <- simDF %>% mutate(temp = 1, A1_copy = A1) %>%
+  #   pivot_wider(names_from = A1_copy, names_prefix = "A1_", values_from = temp) %>%
+  #   mutate_at(vars(-group_cols()), ~replace(., is.na(.), 0)) %>%
+  #   mutate(mu := !!(linearPredictor2(intercept = 0, 
+  #                                    coefs = coefs))) %>%
+  #   mutate(mu = mu + intercept) %>%
+  #   rowwise() %>%
+  #   mutate(Y1 = rnorm(n = 1, mean = mu, sd = sigma)) %>%
+  #   ungroup() %>%
+  #   select(-mu) %>%
+  #   select(-starts_with("A1_"))
   
   return(out)
 }
 
-
+makeDummyHelper <- function(data, A1){
+  dummy_name <- paste("A1", A1, sep = "_")
+  
+  data <- data %>% mutate(!!dummy_name := 1)
+  
+  return(data)
+}
